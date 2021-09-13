4679b6b16065781179058e49c5e13b5276e20460
SuhravHussen <suhravshan@gmail.com>
2021-09-13 19:18:02 +0600

    eslint setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="... ring-4">ring-4</button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <button type="button" className="...  ring-4">
+                ring-4
+            </button>
+        </div>
+    );
 }
 
 export default App;
