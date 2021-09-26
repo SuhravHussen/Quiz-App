7f781a98311c2acffd1f671541152f9caaabb7ea
SuhravHussen <suhravshan@gmail.com>
2021-09-26 17:52:33 +0600

    quiz page progress bar

--- src/Components/Quiz/Answers.js
+++ src/Components/Quiz/Answers.js
@@ -2,48 +2,57 @@ import React from 'react';
 import Checkbox from '../common/Checkbox';
 
 const Answers = () => (
+    // const correct = {
+    //     background: '#4BB543',
+    //     fontWeight: 600,
+    // };
+
+    // const wrong = {
+    //     background: 'red',
+    //     fontWeight: 600,
+    // };
+
     <div className="flex flex-wrap m-2 justify-evenly">
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200 "
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200 "
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200 "
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
         <Checkbox
-            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg"
+            className="w-full sm:w-2/5 my-2.5 p-2.5 bg-indigo-100 rounded-lg hover:bg-blue-200"
             text="Answer is test answer"
         />
     </div>
 );
-
 export default Answers;
--- src/Components/Quiz/ProgressBar.js
+++ src/Components/Quiz/ProgressBar.js
@@ -1,8 +1,37 @@
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Slider from '@mui/material/Slider';
 import React from 'react';
+import './progressBar.css';
 
 const ProgressBar = () => (
-    <div>
-        <h1>Progress Bar</h1>
+    <div className="progressBar bg-green-50 w-11/12 h-16 fixed left-0 right-0 bottom-5 mx-auto my-0 flex justify-between items-center border-green-400 border-opacity-100 border rounded-lg">
+        <div className="backButton w-12 h-12 rounded-lg bg-green-400 flex justify-center items-center cursor-pointer ml-3 hover:bg-green-300 ">
+            <span className="icon">
+                <FontAwesomeIcon icon={faArrowAltCircleLeft} />
+            </span>
+        </div>
+        <div style={{ width: '85%' }}>
+            <Slider
+                color="secondary"
+                disableSwap
+                defaultValue={70}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+            />
+        </div>
+        <a
+            className="p-2 mr-1 bg-green-400 rounded-lg hover:bg-green-300
+        "
+            href="etrtr"
+        >
+            <button type="button" className="button next">
+                <span className="mr-1">Next Question</span>
+                <span className="icon">
+                    <FontAwesomeIcon icon={faArrowAltCircleRight} />
+                </span>
+            </button>
+        </a>
     </div>
 );
 
