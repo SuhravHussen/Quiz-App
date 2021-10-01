1b4120dd899a4331a62d835ccc129a914cd6091b
SuhravHussen <suhravshan@gmail.com>
2021-10-01 20:32:49 +0600

    quiz page miniplayer

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,7 +3,7 @@ import Logo from '../../assets/logo-bg.png';
 import RightContainer from './RightContainer';
 
 const Nav = () => (
-    <ul className=" p-2 flex justify-between items-center bg-green-200">
+    <ul className=" p-2 flex justify-between items-center bg-green-200 shadow-md ">
         <li className="mr-3 flex items-center">
             <img className="mr-3  h-10 sm:ml-3" src={Logo} alt="Learn-with-sumit-logo" />
             <span className="text-xl">Learn with sumit</span>
--- src/Components/Quiz/MiniPlayer.js
+++ src/Components/Quiz/MiniPlayer.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
+import './miniplayer.css';
 
-const MiniPlayer = () => (
-    <div>
-        <h1>MiniPlayer</h1>
-    </div>
-);
+const MiniPlayer = () => {
+    const buttonRef = useRef();
+    const [status, setStatus] = useState(false);
+
+    function toggleMiniPlayer() {
+        if (!status) {
+            buttonRef.current.classList.remove('floatingBtn');
+            setStatus(true);
+        } else {
+            buttonRef.current.classList.add('floatingBtn');
+            setStatus(false);
+        }
+    }
+
+    return (
+        <div
+            role="button"
+            tabIndex={0}
+            ref={buttonRef}
+            onClick={toggleMiniPlayer}
+            className="miniPlayer floatingBtn"
+        >
+            <span className="material-icons-outlined open"> play_circle_filled </span>
+            <span
+                role="button"
+                tabIndex={0}
+                onClick={toggleMiniPlayer}
+                className="material-icons-outlined close"
+            >
+                {' '}
+                close{' '}
+            </span>
+            <ReactPlayer
+                className="player"
+                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                width="300px"
+                height="168px"
+                controls
+                playing={status}
+            />
+            <p>hello world</p>
+        </div>
+    );
+};
 
 export default MiniPlayer;
